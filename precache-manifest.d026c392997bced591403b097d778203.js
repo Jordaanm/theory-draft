6e897cf33c45bd481238a60be1b6686d0ec4e7b0
Jordaan <jordaanm@gmail.com>
2019-06-29 09:31:54 +1000

    Updates

---
+++ precache-manifest.d026c392997bced591403b097d778203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158e3d2260dcb8775c65612efc8ecfec",
+    "url": "/theory-draft/index.html"
+  },
+  {
+    "revision": "e1191cb4f4a59abda024",
+    "url": "/theory-draft/static/css/main.cb371b0a.chunk.css"
+  },
+  {
+    "revision": "e164bc286d28d55079df",
+    "url": "/theory-draft/static/js/2.d65cd4ae.chunk.js"
+  },
+  {
+    "revision": "e1191cb4f4a59abda024",
+    "url": "/theory-draft/static/js/main.ab9cf057.chunk.js"
+  },
+  {
+    "revision": "40d4ecfa90576df4bd08",
+    "url": "/theory-draft/static/js/runtime~main.7f99ac66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/theory-draft/static/media/logo.5d5d9eef.svg"
+  }
+]);
